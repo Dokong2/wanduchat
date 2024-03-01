f29793cf1bc3c1dd490c1b71c4065ce6d8fc2c41
Dokong2 <jiwan6942@gmail.com>
2024-03-01 12:20:38 +0900

    편집각

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 import socket, threading
 connlist = []
+connlist2 = []
 
 def start():
-    global connlist
+    global connlist, connlist2
     host = socket.gethostname()
     server = socket.socket()
     server.bind((host, 1010))
+    bangsong = socket.socket()
+    bangsong.bind((host, 2020))
     print("서버 생성이 완료되었습니다.")
     print("클라이언트 대기를 시작합니다.")
     server.listen(2)
@@ -16,12 +19,10 @@ def start():
         print(f"콘 : {conn}")
         print(f"어드레스 : {addr}")
 
-def clientsee():
-    print("클라이언트 대기를 시작합니다.")
-    
-
-def connsave():
-    
+def clientbangsong(data):
+    global connlist
+    for conn in connlist:
+        conn.send(data.encode())
 
 if __name__ == "__main__":
     start()
