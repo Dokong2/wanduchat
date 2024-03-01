6677e0f189409ae07c3891f5272db978affcb007
Dokong2 <jiwan6942@gmail.com>
2024-03-01 18:28:13 +0900

    방송 서버 기능 재작

--- server.py
+++ server.py
@@ -18,11 +18,21 @@ def start():
         print("클라이언트가 접속했습니다.")
         print(f"콘 : {conn}")
         print(f"어드레스 : {addr}")
+        clientbangsong(f"클라이언트가 접속했습니다. 콘 : {conn}, 어드레스 : {addr}")
+        giveingdatasend(conn=conn)
+
+def giveingdatasend(conn):
+    while True:
+        bangsongdata = conn.recv(1024).decode()
+        bangsongdata = str(bangsongdata)
+        clientbangsong(bangsongdata)
+        print(f"방송서버에 데이터가 와 클라이언트에게 전송합니다.. 받은 데이터 : {bangsongdata}")
 
 def clientbangsong(data):
-    global connlist
-    for conn in connlist:
+    global connlist2
+    for conn in connlist2:
         conn.send(data.encode())
+    print(f"모든 클라이언트에게 메시지 [{data}]가 전송되었습니다.")
 
 if __name__ == "__main__":
     start()
